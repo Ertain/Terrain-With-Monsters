846b9a25c4aa23b074e0ca120a21a9428d10377e
Jason Anderson <sirius-C@iname.com>
2018-04-15 23:16:51 -0500

    Added an exit option

---
+++ Main.gd
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 Jason Anderson
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+# 
+#
+
+
+extends Node2D
+
+# Some code for exiting the program.
+func _process(change_in_time):
+    if(Input.is_key_pressed(KEY_ESCAPE)):
+        get_tree().quit()
